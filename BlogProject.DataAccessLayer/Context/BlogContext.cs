56fd3ff0a2a9d4890bb2988d44fb7602a9b7c8c4
emredaglar <emre.daglar1@gmail.com>
2024-11-22 00:09:17 +0300

    Add project files.

---
+++ BlogProject.DataAccessLayer/Context/BlogContext.cs
@@ -0,0 +1,25 @@
+﻿using BlogProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogProject.DataAccessLayer.Context
+{
+    internal class BlogContext : IdentityDbContext<AppUser, AppRole, int>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DD\\SQLEXPRESS01;initial Catalog=BlogDb;integrated security=true;TrustServerCertificate=True");
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Newsletter> Newsletters { get; set; }
+        public DbSet<TagCloud> TagClouds { get; set; }
+    }
+}
---
+++ BlogProject.EntityLayer/Concrete/Article.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogProject.EntityLayer.Concrete
+{
+    public class Article
+    {
+        public int ArticleId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string CoverImageUrl { get; set; }
+        public int CategoryId { get; set; }
+        public Category category { get; set; }
+        public int AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
+        public List<Comment> Comments { get; set; }
+
+    }
+}
