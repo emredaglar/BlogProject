b1c166b91dd5d75642782f5657436752d64ae400
emredaglar <emre.daglar1@gmail.com>
2024-11-23 14:50:24 +0300

    added GenericDal

--- BlogProject.DataAccessLayer/Context/BlogContext.cs
+++ BlogProject.DataAccessLayer/Context/BlogContext.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace BlogProject.DataAccessLayer.Context
 {
-    internal class BlogContext : IdentityDbContext<AppUser, AppRole, int>
+    public class BlogContext : IdentityDbContext<AppUser, AppRole, int>
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
---
+++ BlogProject.DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,49 @@
+﻿using BlogProject.DataAccessLayer.Abstract;
+using BlogProject.DataAccessLayer.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogProject.DataAccessLayer.Repositories
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        private readonly BlogContext _blogContext;
+
+        public GenericRepository(BlogContext blogContext)
+        {
+            _blogContext = blogContext;
+        }
+
+        public void Delete(int id)
+        {
+            var value = _blogContext.Set<T>().Find(id);
+            _blogContext.Set<T>().Remove(value);
+            _blogContext.SaveChanges();
+        }
+
+        public List<T> GetAll()
+        {
+            return _blogContext.Set<T>().ToList();
+        }
+
+        public T GetById(int id)
+        {
+            return _blogContext.Set<T>().Find(id);
+        }
+
+        public void Insert(T entity)
+        {
+            _blogContext.Set<T>().Add(entity);
+            _blogContext.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            _blogContext.Set<T>().Update(entity);
+            _blogContext.SaveChanges();
+        }
+    }
+}
