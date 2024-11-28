77ce83263ee7833a3bf786f5698575b59b3f8010
emredaglar <emre.daglar1@gmail.com>
2024-11-28 12:35:53 +0300

    added Newsletter

---
+++ BlogProject.PresentationLayer/Controllers/NewsletterController.cs
@@ -0,0 +1,28 @@
+﻿using BlogProject.BusinessLayer.Abstract;
+using BlogProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogProject.PresentationLayer.Controllers
+{
+    public class NewsletterController : Controller
+    {
+        private readonly INewsletterService _newsletterService;
+
+        public NewsletterController(INewsletterService newsletterService)
+        {
+            _newsletterService = newsletterService;
+        }
+        [HttpGet]
+        public PartialViewResult NewsletterSubscribe()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public IActionResult NewsletterSubscribe(Newsletter newsletter)
+        {
+          
+            _newsletterService.TInsert(newsletter);
+            return RedirectToAction("Index","Default");
+        }
+    }
+}
--- BlogProject.PresentationLayer/ViewComponents/_NewsletterCP.cs
+++ BlogProject.PresentationLayer/ViewComponents/_NewsletterCP.cs
@@ -1,11 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BlogProject.BusinessLayer.Abstract;
+using BlogProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 namespace BlogProject.PresentationLayer.ViewComponents
 {
     public class _NewsletterCP : ViewComponent
     {
+        private readonly INewsletterService _newsletterService;
+
+        public _NewsletterCP(INewsletterService newsletterService)
+        {
+            _newsletterService = newsletterService;
+        }
         public IViewComponentResult Invoke()
         {
             return View();
         }
+        
     }
 }
