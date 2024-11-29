6cdc4939f1e65e2a99cd94020af3337c5e22d890
emredaglar <emre.daglar1@gmail.com>
2024-11-29 15:22:52 +0300

    added Pagging

--- BlogProject.PresentationLayer/ViewComponents/_BlogsCP.cs
+++ BlogProject.PresentationLayer/ViewComponents/_BlogsCP.cs
@@ -14,9 +14,27 @@ namespace BlogProject.PresentationLayer.ViewComponents
 
         public IViewComponentResult Invoke()
         {
-            var values = _articleService.TBlogsWithUserAndComment();
-            return View(values);
-        }
+            int page = 1;
+            int pageSize = 2; // Sayfa başına gösterilecek makale sayısı
 
+            // URL'deki 'page' parametresini al
+            if (HttpContext.Request.Query.TryGetValue("page", out var pageValue))
+            {
+                int.TryParse(pageValue, out page);
+            }
+
+            // Makaleleri getir ve sayfalama uygula
+            var allArticles = _articleService.TBlogsWithUserAndComment();
+            var pagedArticles = allArticles
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+
+            // Toplam sayfa sayısını hesapla
+            ViewBag.TotalPages = (int)Math.Ceiling((double)allArticles.Count() / pageSize);
+            ViewBag.CurrentPage = page;
+
+            return View(pagedArticles);
+        }
     }
 }
