fd44f52842095f257016930e36a33a8b2e93f3a7
emredaglar <emre.daglar1@gmail.com>
2024-11-30 15:38:42 +0300

    added prev and next post

--- BlogProject.PresentationLayer/Controllers/ArticleDetailController.cs
+++ BlogProject.PresentationLayer/Controllers/ArticleDetailController.cs
@@ -17,8 +17,31 @@ namespace BlogProject.PresentationLayer.Controllers
         {
             var article = _articleService.TGetById(id);
             ViewBag.ArticleId = article.ArticleId;
+
+            //Önceki ve Sonraki Bloglar----------------------------------------------
+            var prevArticle = _articleService.TGetAll()
+                .Where(a => a.ArticleId < id)
+                .OrderByDescending(a => a.ArticleId)
+                .FirstOrDefault();
+
+            var nextArticle = _articleService.TGetAll()
+                .Where(a => a.ArticleId > id)
+                .OrderBy(a => a.ArticleId)
+                .FirstOrDefault();
+
+            ViewBag.PrevArticle = prevArticle;
+            ViewBag.NextArticle = nextArticle;
+
+            //Önceki ve Sonraki Bloglar----------------------------------------------
+
+
+            // Newsletter nesnesi oluşturuluyor ve ViewBag'e ekleniyor
+            var newsletter = new Newsletter();
+            ViewBag.Newsletter = newsletter;
+
             var value = _articleService.TArticleDetailWithUserAndComment(id);
-            return View(value);
+            return View(value);  // Article detail ve yorumları içeriyor
         }
+
     }
 }
