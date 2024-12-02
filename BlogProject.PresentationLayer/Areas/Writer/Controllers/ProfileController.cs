3859313ebac33cdc7382fda4ab07c4a2839fdc4d
emredaglar <emre.daglar1@gmail.com>
2024-12-02 23:30:50 +0300

    added Writer Dashboard-Profile

---
+++ BlogProject.PresentationLayer/Areas/Writer/Controllers/ProfileController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogProject.PresentationLayer.Areas.Writer.Controllers
+{
+    [Area("Writer")]
+    public class ProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
